a9f4b1859d8bb5fa8e2f7d36ecd9637a46a90656
William <contato.williamc@gmail.com>
2020-11-10 12:07:53 -0300

    Jogo da Velha Finalizado

--- EXERCICIOS/AULA-04-11-2020/jogo_velha/jogo_velha.py
+++ EXERCICIOS/AULA-04-11-2020/jogo_velha/jogo_velha.py
@@ -1,116 +1,263 @@
-from os import system, name 
-from typing import Tuple
+from configparser import ConfigParser
+from typing import List, Tuple
+import os
 
 
-palavra = ''
-chances = 0
+SIMBOLOS = '!"#$%&\'()*+,./:;<=>?@[]^_`{|}~'
 
-def is_digit(letter: str) -> bool:
-    """
-        Recebe uma letra e verifica se ela é um dígito
-
-        Attr
-    """
-    try:
-        float(letter)
-        return False
-    except ValueError:                
-        return True
-    
 
 def get_config() -> Tuple[str, int]:
-    global palavra, chances
-    with open('conf.cfg') as file:
-        palavra = file.readline().strip()
-        chances = int(file.readline())
+    """Obtém as configurações do jogo através do arquivo config.cfg.
+
+    Returns:
+        Caso o arquivo de configuração exista, retorna uma tupla que
+        corresponde a palavra e as chances do jogador, senão retorna
+        os valores DEFAULT.
+    """
+    try:
+        # Abrindo o arquivo de configuração em modo de leitura.
+        with open('config.cfg', encoding='utf8') as config_file:
+            # Instanciando um ConfigParser.
+            config = ConfigParser()
+            # Definindo o arquivo que vai ser lido.
+            config.read_file(config_file)
+            # Verificando se existe a seção USER_SPECIFIED no arquivo de
+            # configuração.
+            if config.has_section('USER_SPECIFIED'):
+                # Obtendo os valores da seção USER_SPECIFIED.
+                word = config.get('USER_SPECIFIED', 'word')
+                chances = config.getint('USER_SPECIFIED', 'chances')
+            else:
+                # Obtendo os valores da seção DEFAULT.
+                word = config.get('DEFAULT', 'word')
+                chances = config.getint('DEFAULT', 'chances')
+
+            return word, chances
+    except FileNotFoundError:
+        # Imprimindo as mensagens de erro.
+        print('Não existe um arquivo de configuração.')
+        print('Serão utilizados valores DEFAULT.')
+        # Pausa para o usuário poder ler as mensagens.
+        input('Digite qualquer tecla para continuar: ')
+        # Criando o arquivo com dados DEFAULT.
+        set_config()
+        # Abrindo o arquivo de configuração em modo de leitura.
+        with open('config.cfg') as config_file:
+            # Instanciando o ConfigParser.
+            config = ConfigParser()
+            # Lendo o arquivo de configurações.
+            config.read_file(config_file)
+            # Obtendo os valores do arquivo.
+            word = config.get('DEFAULT', 'word')
+            chances = config.getint('DEFAULT', 'chances')
+
+            return word, chances
 
 
-def limpa_tela() -> None:
-    # for windows 
-    if name == 'nt': 
-        system('cls') 
-    # for mac and linux(here, os.name is 'posix') 
+def clear_screen() -> None:
+    """Limpa o console.
+
+    Verifica qual o sistema operacional e executa o comando necessário
+    para limpar o console naquele sistema.
+    """
+    if os.name == 'nt':
+        os.system('cls')
     else:
-        system('clear')   
+        os.system('clear')
 
 
-def has_repeated_letter(letra: str, tentativa_palavra: str) -> bool:
-    return letra in tentativa_palavra
+def is_valid_guess(guess: str, letters_used: List[str]) -> bool:
+    """Valida o chute.
+
+    Verifica se o chute é um digito, símbolo ou se ele já foi digitado.
+
+    Args:
+        guess: chute.
+        letters_used: letras já utilizadas.
+
+    Returns:
+        False se for um dígito, já tiver sido utilizado ou for um símbolo,
+        senão retorna True.
+    """
+    # Definindo as validações que seram feitas.
+    validations = [
+        {
+            'validation_name': 'is_digit',
+            'validation': guess.isdigit(),
+            'error_message': 'o chute é um número'
+        },
+        {
+            'validation_name': 'already_used',
+            'validation': guess in letters_used,
+            'error_message': 'letra já usada'
+        },
+        {
+            'validation_name': 'is_symbol',
+            'validation': guess in SIMBOLOS,
+            'error_message': 'caractere inválido'
+        },
+        {
+            'validation_name': 'invalid_lenght',
+            'validation': len(guess) != 1,
+            'error_message': 'só é permitido um caractere por chute'
+        }
+    ]
+    # Fazendo todas as validações e imprimindo a mensagem de erro.
+    for validation in validations:
+        if validation['validation']:
+            print(f'Erro ({validation["validation_name"]}): '
+                  f'{validation["error_message"]}.')
+            return False
+
+    return True
 
 
-def verifica_fim_jogo(tentativa_palavra: str) -> bool:
-    global palavra, chances
+def game_over_check(valid_guesses: str, word: str, chances: int) -> bool:
+    """Verifica se o jogo acabou.
 
-    if tentativa_palavra == palavra:
-            print(f"Você ganhou com apenas {chances}!")
-            return True
-    
+    O jogo acaba quando o usuário acertar a palavra, ou as chances chegarem
+    a zero.
+
+    Args:
+        valid_guesses: palavra formada pelo usuário.
+        word: palavra-chave do jogo.
+        chances: quantidade de chances restantes.
+
+    Returns:
+        True caso o jogo tenha acabado, False caso contrário.
+    """
+    if valid_guesses == word:
+        clear_screen()
+        print(f'A palavra era: {word}.')
+        print(f'Você ganhou com {chances} chance(s) sobrando!')
+        input('Pressione RETURN para voltar ao menu principal.')
+        return True
+
     if chances == 0:
-        print("Você perdeu!")
+        clear_screen()
+        print(f'A palavra era: {word}.')
+        print('Suas chances acabaram. Você perdeu :(')
+        input('Pressione RETURN para voltar ao menu principal.')
         return True
 
 
-def print_menu() -> None:
+def play() -> None:
+    """Função responsável por controlar o jogo."""
+    word, chances = get_config()
+    letters_used = []
     while True:
-        print('1 - Jogar')
-        print('2 - Modificar as configurações')
-        print('3 - Sair')
-        opcao = input('Digite a opção escolhida: ')
+        clear_screen()
+        valid_guesses = ''
 
-        if opcao in ('1', '2', '3'):
-            return opcao
-        else:
-            print('Opção inválida')
+        # Printando o jogo.
+        print(f'Chances disponíveis: {chances}.')
+        print(*letters_used, end='\n\n')
+        print(valid_guesses)
+        for letter in word:
+            valid_guesses += letter if letter in letters_used else '_'
+        print(valid_guesses, end='\n\n')
 
-def jogar():  
-    global palavra, chances 
-    get_config()   
+        # Verificando se o jogo acabou.
+        game_over = game_over_check(valid_guesses, word, chances)
 
-    letras_usadas = []
-    while True:
-        tentativa_palavra = ""
- 
-        limpa_tela()                    
-        print("Número de chances: %d - tentativas:" % chances) # interpolação de string
-        print(*letras_usadas) # imprime item por item do array
-        print("\n")
-        print(tentativa_palavra)
-        for x in palavra:
-            tentativa_palavra += x if x in letras_usadas else "_"
-        print(tentativa_palavra + "\n\n")        
- 
-        fim_jogo = verifica_fim_jogo(tentativa_palavra)
-
-        if fim_jogo:
+        if game_over:
             break
 
+        # Obtendo o valor do chute.
         while True:
-            chute = input("Digite uma letra:")
-            
-            chute_valido = is_digit(chute)
+            guess = input('Digite uma letra: ').lower()
 
-            if not chute_valido:            
-                print('Dígito inválido')
-                continue
+            clear_screen()
 
-            letra_repetida = has_repeated_letter(chute, letras_usadas)        
+            # Verificando se o chute é válido.
+            if is_valid_guess(guess, letters_used):
+                break
 
-            if letra_repetida:
-                print('Letra repetida') 
-            else:
-                break                            
+        # Adicionando o chute na lista de chutes.
+        letters_used.append(guess)
 
-        letras_usadas.append(chute)
-
-        if not chute in palavra:
+        # Diminuindo as chances caso o chute seja inválido.
+        if guess not in word:
             chances -= 1
 
 
-def main() -> None:
-    menu = print_menu()
+def main_menu() -> str:
+    """Imprime o menu principal.
 
-    if menu == '1':
-        jogar()
-        
-if __name__ == "__main__":
+    Returns:
+        A opção escolhida pelo usuário.
+    """
+    menu_options = {
+        '1': 'Jogar',
+        '2': 'Modificar as configurações',
+        '3': 'Sair'
+    }
+
+    while True:
+        print('*' * 30)
+        print('Escolha uma opção:')
+        for option, value in menu_options.items():
+            print(f'{option} - {value}')
+
+        print('*' * 30)
+        choosed_option = input('Digite a opção desejada: ')
+
+        if choosed_option in menu_options.keys():
+            return choosed_option
+        else:
+            clear_screen()
+            print('Opção inválida.')
+
+
+def set_config(word: str = None, chances: str = None) -> None:
+    """Modifica as configurações do jogo.
+
+    Se os parâmetros forem passados, cria uma seção USER_SPECIFIED no
+    arquivo com esses valores, caso contrário só cria os valores
+    DEFAULT.
+
+    Args:
+        word: palavra do jogo.
+        chances: quantidade de chances.
+    """
+
+    # Instanciando um ConfigParser.
+    config = ConfigParser()
+    # Definindo os valores DEFAULT do arquivo.
+    config['DEFAULT'] = {
+        'word': 'python',
+        'chances': '3'
+    }
+    # Verificando se os parâmetros foram passados.
+    if word and chances:
+        # Criando a seção USER_SPECIFIED e adicionando os atributos.
+        config['USER_SPECIFIED'] = {
+            'word': word,
+            'chances': chances
+        }
+
+    # Escrevendo os valores no arquivo.
+    with open('config.cfg', 'w', encoding='utf8') as config_file:
+        config.write(config_file)
+
+
+def main() -> None:
+    while True:
+        # Recebe a opção do menu principal.
+        menu_option = main_menu()
+        clear_screen()
+        # Controle de fluxo do programa.
+        if menu_option == '1':
+            play()
+        elif menu_option == '2':
+            print('Modificando as configurações.')
+            word = input('Digite a palavra desejada: ').lower()
+            changes = input('Digite a quantidade de chances desejada: ')
+            set_config(word, changes)
+        else:
+            print('Obrigado por jogar!')
+            break
+
+
+if __name__ == '__main__':
     main()
